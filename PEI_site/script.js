bf95a7cabc87979c9d1fea34d29d07f876d2e419
andreturner <99768590+andreturner@users.noreply.github.com>
2022-11-18 03:58:05 -0500

    Add files via upload

---
+++ PEI_site/script.js
@@ -0,0 +1,44 @@
+//at the beginning of jquery script
+//wait for page to load before executing code
+window.onload = function(){
+
+  // target image to be replaced using its class
+  //Land Use pics//
+  $('.cavendish-image').click(function(){
+    $(this).attr('src', 'pictures/3_aerial/aerial_pics/2010_cavendish.jpeg');
+  }); 
+
+  $('.greenwich-image').click(function(){
+    $(this).attr('src', 'pictures/3_aerial/aerial_pics/2010_greenwich.jpeg');
+  }); 
+
+//Ecosystem pics//
+$("#owl-demo").owlCarousel({
+ 
+
+
+  slideSpeed : 300,
+  paginationSpeed : 400,
+
+  items : 1, 
+  itemsDesktop : false,
+  itemsDesktopSmall : false,
+  itemsTablet: false,
+  itemsMobile : false
+
+});
+
+  $('.marram-grass').click(function(){
+    $(this).attr('src', 'pictures/4_ecosystem/ecosystem_pics/grass_cliff.png');
+  });
+  
+  $('.grass-cliff').click(function(){
+    $(this).attr('src', 'pictures/4_ecosystem/ecosystem_pics/sandstone.png');
+  });
+  
+  $('.sandstone').click(function(){
+    $(this).attr('src', 'pictures/4_ecosystem/ecosystem_pics/sandstone_cliff.jpeg');
+  });
+
+
+}
